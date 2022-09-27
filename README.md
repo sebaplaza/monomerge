053f9782bec8b25b86b2ffd7607b0abcf77a6a4d
Sebastian Plaza <sebastianplaza@gmail.com>
2022-09-27 13:27:15 +0200

    feat: first version

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # monomerge
+
+Merge multiple json coverage files into one, then use nyc to create a merged report. Ideal for monorepos.
+
+## How to
+
+```shell
+npm add -D monomerge nyc
+```
+
+```shell
+npx monomerge
+```
+
+This command will find all your `coverage-final.json` files into your `packages/**` folders.
+
+All these files will be merged into `.nyc_output/coverage.json` file.
+
+Now you can do whathever you want with this merged coverage file.
+
+## Generate a report
+
+Just call nyc with your prefered Options
+
+```shell
+npx nyc report --reporter=html --reporter=lcov
+# or via a nyc config file https://github.com/istanbuljs/nyc#configuration-files
+```
---
+++ src/coverage-collect.js
@@ -0,0 +1,35 @@
+const fs = require("fs-extra");
+const fg = require("fast-glob");
+const util = require("util");
+const exec = util.promisify(require("child_process").exec);
+
+(async () => {
+	const entries = await fg(["packages/**/coverage-final.json"], {
+		dot: true,
+		absolute: true,
+	});
+
+	await fs.emptyDir("coverage");
+	await fs.emptyDir(".nyc_output");
+
+	const promises = entries.map((entry) => {
+		console.log(`coverage file found: ${entry}`);
+		const rgPackageName = new RegExp(
+			"packages/(.*)/coverage/coverage-final.json"
+		);
+		const [, packageName] = rgPackageName.exec(entry);
+		const targetPath = `coverage/${packageName}.json`;
+		console.log(`copy to: ${targetPath}`);
+		return fs.copyFile(entry, targetPath);
+	});
+	await Promise.all(promises);
+
+	const { stdout, stderr } = await exec(
+		"npx nyc merge coverage .nyc_output/coverage.json"
+	);
+	console.log(`stdout: ${stdout}`);
+
+	console.log(
+		`now you can generate your reports with: npx nyc report. More info: https://istanbul.js.org/docs/advanced/coverage-object-report/ `
+	);
+})();
